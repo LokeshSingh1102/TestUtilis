7b808d63d096a78f128021d4da60f183e6336c13
LokeshSingh1102 <lokeshsinghbora1102@gmail.com>
2022-10-18 20:58:05 +0530

    Updates

---
+++ precache-manifest.465eebcf89baaa78c12c3d07b0c0550f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "caa152c374a89bda8e3f",
+    "url": "/TestUtilis/static/css/main.c532e5d3.chunk.css"
+  },
+  {
+    "revision": "caa152c374a89bda8e3f",
+    "url": "/TestUtilis/static/js/main.caa152c3.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TestUtilis/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TestUtilis/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "92dc98c597a255ad196a",
+    "url": "/TestUtilis/static/js/runtime~main.92dc98c5.js"
+  },
+  {
+    "revision": "1648f5cfb0f27c72b9541839477784ad",
+    "url": "/TestUtilis/index.html"
+  }
+];
