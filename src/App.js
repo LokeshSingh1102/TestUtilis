e8eccfc33798f3fdf16a7f94d495fd7914d7228b
LokeshSingh1102 <lokeshsinghbora1102@gmail.com>
2022-10-18 20:29:23 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import Alert from './components/Alert';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-function App() {
+
+function App() { //this will return only one component
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey'
+      showAlert('Dark mode has been enabled', 'success')
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert('Light mode has been enabled', 'success')
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title="TextUtils" Home="Home" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className='container'>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Textform mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+
+    // fragment tag under this we can have multiple component <></>
+    //component -> A collection of tags under a tag 
+    // <>
+    // <nav>
+    //   <li>home</li>
+    //   <li>home</li>
+    // </nav>
+    // </>
   );
 }
 
